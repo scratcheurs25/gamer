6c741c572e8af863b0004701827d731f343c400c
scratcheurs25 <126276357+scratcheurs25@users.noreply.github.com>
2023-04-11 15:49:38 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+input.onButtonPressed(Button.A, function () {
+    Gamer.moov(Move.Reculer, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    Gamer.moov(Move.Avecer, 1)
+})
+Gamer.barlaterale(4)
+Gamer.player(2, 3)
